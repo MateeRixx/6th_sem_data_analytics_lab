f04875700df7ea0171e9b33ff17e9ef9e285add1
Mohit Kumar <23IT3028@rgipt.ac.in>
2026-01-12 12:47:08 +0530

    task1 done

---
+++ lab_1_task-1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double mean(vector<int>records,int scale){
+    
+}
+
+int main()
+{
+
+
+    int number=1000;
+    vector<int> year_records={45, 52, 48, 55, 60, 58, 62, 65, 70, 68, 72, 150};
+
+    
+
+   
+
+    
+
+
+
+
+}
